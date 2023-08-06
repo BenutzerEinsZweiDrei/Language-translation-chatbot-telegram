7e9f7ccd5813380d839c22aa60c4bad8498bda06
BenutzerEinsZweiDrei <57397036+BenutzerEinsZweiDrei@users.noreply.github.com>
2023-08-06 18:46:42 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,16 +13,12 @@ learnLanguage = "French" # bot will be prompted to teach that language. Put here
 langCode1 = "fr" # the first message shows up in
 langCode2 = "en" # the second message shows up in
 
-lang = True # True if your message should be translated for hugchat or set to False if not. 
-inmessage = False # remind bot to act as a teacher before every answer with True
-
-BOT_TOKEN = "your bot token"
+BOT_TOKEN = "<your telegram bot token>"
 
 # Teacher Prompt
 # will be translated to your langCode1
 
-promptTeacher = "I want you to act as a spoken "+learnLanguage+" teacher and improver. I will speak to you in "+learnLanguage+" and you will reply to me in "+learnLanguage+" to practice my spoken "+learnLanguage+". I want you to keep your reply neat, limiting the reply to 100 words. I want you to strictly correct my grammar mistakes, typos, and factual errors. I want you to ask me a question in your reply. Now let's start practicing, you could ask me a question first. Remember, I want you to strictly correct my grammar mistakes, typos, and factual errors."
-
+promptTeacher = "I want you to act as a spoken "+learnLanguage+" teacher and improver for the prompt at the end of the message. I will speak to you in "+learnLanguage+" and you will reply to me in "+learnLanguage+" to practice my spoken "+learnLanguage+". I want you to keep your reply neat, limiting the reply to 100 words. I want you to strictly correct my grammar mistakes, typos, and factual errors. I want you to ask me a question in your reply. Now let's start practicing, you could ask me a question first. Remember, I want you to strictly correct my grammar mistakes, typos, and factual errors and please remember to keep your answer short and in easy language. Remember all of this but write your answer only to the following prompt '"
 
 # load hugchat
 if debug : 
@@ -34,6 +30,7 @@ if debug :
 if debug : 
     print("DEBUG: before starting conversation")
 # Create a new conversation
+chatbot.switch_llm(1) # use llama
 id = chatbot.new_conversation()
 chatbot.change_conversation(id)
 
@@ -53,9 +50,6 @@ if debug :
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, im a language translation bot. Just keep talking to me.")
-    # tell bot to act as a teacher with every start command
-    translated_prompt = GoogleTranslator(source='auto', target= langCode1 ).translate(promptTeacher) 
-    chatbot.chat(translated_prompt)
     if debug : 
         print("DEBUG: processed start command")
     
@@ -63,31 +57,26 @@ def send_welcome(message):
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     if debug : 
-        print("DEBUG: Start of message")
+        print("DEBUG: Start of echo_all")
     # tell bot to act as a language teacher before every message.
-    if inmessage :
-        translated_prompt = GoogleTranslator(source='auto', target= langCode1 ).translate(promptTeacher) 
-        chatbot.chat(translated_prompt)
-        if debug : 
-            print("DEBUG: chatbot got instructions")
-    if lang :
-        translate_message = GoogleTranslator(source='auto', target= langCode1 ).translate(message.text) # just ask in french so he answers in french
-    else :
-    	translate_message = message.text
+    prompt = promptTeacher + message.text + "'"
+    translated_prompt = GoogleTranslator(source='auto', target= langCode1 ).translate(prompt) 
     if debug : 
-        print("DEBUG: Googletranslator translated")
-    response = chatbot.chat(translate_message)
-    response_str = str(response) # try to fix doctype html crash
-    bot.reply_to(message, flag1 + ' ' +response_str)
+            print("DEBUG: prompt translated")
+    response = chatbot.chat(translated_prompt)
+    if debug :
+    	    print("DEBUG: received hugchat response")
+    bot.reply_to(message, flag1 + ' ' +response)
     if debug : 
         print("DEBUG: bot replied first message")
-    translated = GoogleTranslator(source='auto', target= langCode2 ).translate(response_str) 
+    # into second language
+    translated = GoogleTranslator(source='auto', target= langCode2 ).translate(response) 
     bot.reply_to(message,  flag2 + ' ' +translated)
     if debug : 
          print("DEBUG: bot replied second message. End of message function")
 
 if debug : 
-    print("DEBUG: before infinity polling")
+    print("DEBUG: before infinity polling. Start messaging.")
 # bot.polling(none_stop=True) # check which one works better
 bot.infinity_polling()
 
